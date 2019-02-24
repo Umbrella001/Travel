6b3908daba0b59e5db9df4c57a485d403ec179f1
Umbrella <13798972826@163.com>
2019-02-25 00:07:58 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
+           /*main.js项目入口文件：需要的插件，引用都从这边导入*/
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//reset样式表就是重置不同手机浏览器默认样式使其统一，手机端必备！
+//border解决手机端多倍屏对1px物理像素边框的纠正
+//fastclick解决移动端click事件延迟300ms的问题
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+/*导入fastclick插件后，使用该插件的attach添加到body层即可*/
+fastClick.attach(document.body);
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-})
+});
