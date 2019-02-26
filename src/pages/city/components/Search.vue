65bacd5b70ea2d98ea32f886a58c33e674d18faa
Umbrella <13798972826@163.com>
2019-02-27 00:58:22 +0800

    city search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="search">
+  <input class="search-input" type="text" placeholder="输入城市名或拼音">
+</div>
+</template>
+
+<script>
+    export default {
+        name: "CitySearch"
+    }
+</script>
+s
+<style lang="stylus" scoped>
+  @import "~styles/variable.styl"
+.search
+  height : 36px
+  background :$bgColor
+  padding :0 10px
+.search-input
+  width :100%
+  height :30px
+  line-height:30px
+  padding :0 8px
+  border-radius : 3px
+  color : $textColor
+  text-align :center
+  box-sizing :border-box
+</style>
