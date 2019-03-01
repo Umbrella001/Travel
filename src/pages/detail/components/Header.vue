3aec9e158b185fd7e9f042b2c6272efdeaa8d728
Umbrella <13798972826@163.com>
2019-03-01 15:00:43 +0800

    detail-header

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -24,6 +24,9 @@
       activated(){
           window.addEventListener('scroll',this.scrollMove)
       },
+      deactivated(){
+        window.removeEventListener('scroll',this.scrollMove)
+      },
       methods:{
         scrollMove(){
           const top = document.documentElement.scrollTop;
@@ -41,9 +44,10 @@
    * 功能：实现冻结栏随着scroll的移动距离出现渐隐渐现的效果
    * 思路1：定义对象的v-show事件进行两个状态的管理
    * 思路2：定义mounted/activated（注意activated并不是vue里的，而是vuex中keep-alive内新增的钩子）来监听scroll的事件，在事件中函数中读取滚动距离值
-   * 监听scroll: 钩子中使用window.addEventListener对scroll添加一个监听事件,调用事件的函数
-   * 获取scroll滚动值实现展示效果：定义一个const函数占存参数top存储为document.documentElement.scrollTop这个滚动值，使用判断进行v-show的展示
-   * 获取scroll滚动值实现渐现效果：使用var/let非函数占存参数opacity,并三元固定正确的opacity值区间,最后将opacity对象传给（:style="参数"）参数
+   * 监听scroll>>> 钩子中使用window.addEventListener对scroll添加一个监听事件,调用事件的函数
+   * 获取scroll滚动值实现展示效果>>>定义一个const函数占存参数top存储为document.documentElement.scrollTop这个滚动值，使用判断进行v-show的展示
+   * 获取scroll滚动值实现渐现效果>>>使用var/let非函数占存参数opacity,并三元固定正确的opacity值区间,最后将opacity对象传给（:style="参数"）参数
+   * 结束scroll值获取的全局window>>>使用window.removeEventListener解除window的全局绑定其他页面scroll读值的影响
    */
   @import "~styles/variable.styl"
 .round
